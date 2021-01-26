019b03ec2f2afb4561ca385d84f003a603e27153
masterzenith <sajibghoshbig@gmail.com>
2021-01-27 06:16:40 +0900

    fast and slow pointers

---
+++ src/main/java/fastslowpointers/HappyNumber.java
@@ -0,0 +1,37 @@
+package fastslowpointers;
+
+/**
+ * Input: 23
+ * Output: true (23 is a happy number)
+ * Explanations: Here are the steps to find out that 23 is a happy number:
+ */
+public class HappyNumber {
+    public static boolean find(int num) {
+        int slow = num, fast = num;
+        do {
+            slow = findSquareSum(slow); // move one step
+            fast = findSquareSum(findSquareSum(fast)); // move two steps
+        } while (slow != fast); // found the cycle
+
+        return slow == 1; // see if the cycle is stuck on the number '1'
+    }
+
+    private static int findSquareSum(int num) {
+        int sum = 0, digit;
+        while (num > 0) {
+            digit = num % 10;
+            sum += digit * digit;
+            num /= 10;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(HappyNumber.find(23));
+        System.out.println(HappyNumber.find(12));
+    }
+}
+/**
+ * Time: O(logN)
+ * Space: O(1)
+ */
